c9554a1d5175c95d89299d6e0b8395c9a2e57a87
devwqc <devwqc@gmail.com>
2024-05-20 17:00:57 +0900

    chore: global css 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['styles'],
+    additionalData: `@import "src/styles/globals.scss";`,
+  },
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import styles from './Home.module.scss';
+
+export default function HomePage() {
+  return <h1 className={styles.home}>HomePage</h1>;
 }
