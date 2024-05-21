ddca86f027babfbf1df54bd0541e6bb38620fdf8
devwqc <devwqc@gmail.com>
2024-05-21 15:06:03 +0900

    refactor: Nav 더미 데이터 경로 수정

--- src/components/common/Nav/index.tsx
+++ src/components/common/Nav/index.tsx
@@ -4,13 +4,13 @@ import styles from './Nav.module.scss';
 
 const DUMMY_MENUS = [
   { id: 1, title: 'Home', url: '/' },
-  { id: 2, title: '인기상품', url: '/products/2' },
-  { id: 3, title: 'NEW신상', url: '/products/3' },
-  { id: 4, title: '특가상품', url: '/products/4' },
-  { id: 5, title: '샘플', url: '/products/5' },
-  { id: 6, title: '비밀상점', url: '/products/6' },
-  { id: 7, title: '최저가', url: '/products/7' },
-  { id: 8, title: '이벤트', url: '/events' },
+  { id: 2, title: '인기상품', url: '/test/products/2' },
+  { id: 3, title: 'NEW신상', url: '/test/products/3' },
+  { id: 4, title: '특가상품', url: '/test/products/4' },
+  { id: 5, title: '샘플', url: '/test/products/5' },
+  { id: 6, title: '비밀상점', url: '/test/products/6' },
+  { id: 7, title: '최저가', url: '/test/products/7' },
+  { id: 8, title: '이벤트', url: '/test/events' },
 ];
 
 export default function Nav() {
