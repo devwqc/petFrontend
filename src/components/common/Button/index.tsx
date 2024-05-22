a0efbe6a16f4be324e46f3b25d7b8cd4531bece2
seolsis <seoljw1005@naver.com>
2024-05-22 16:56:32 +0900

    feat: 버튼 공통 컴포넌트 생성

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,3 +1,23 @@
-export default function Button() {
-  return <div>Button</div>;
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  size: 'large' | 'medium';
+  text: string;
+  backgroundColor: '#545454' | '#FFFFFF';
+  onClick?: () => void;
+}
+
+export default function Button({ size, text, backgroundColor, onClick }: ButtonProps) {
+  const sizeClass = styles[size];
+  const backgroundClass = backgroundColor === '#545454' ? styles.backgroundBlack : styles.backgroundWhite;
+  const className = `${styles.button} ${sizeClass} ${backgroundClass}`;
+
+  return (
+    <>
+      <div>Button</div>
+      <button className={className} onClick={onClick}>
+        {text}
+      </button>
+    </>
+  );
 }
