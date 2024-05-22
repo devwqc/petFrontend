1d5be3201cadd9dd342fa46a2e30e547596fdc1e
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-22 21:40:10 +0900

    feat: 인풋 레이아웃 구현

--- src/components/common/Input/InputLayout.tsx
+++ src/components/common/Input/InputLayout.tsx
@@ -1,3 +1,4 @@
+import Image, { ImageProps } from 'next/image';
 import classNames from 'classnames/bind';
 import styles from './Input.module.scss';
 
@@ -6,11 +7,14 @@ interface InputProps {
   type: string;
   label?: string;
   value?: string | number;
-  style?: string;
+  size?: string;
+  border?: string;
   isError: boolean;
   errorText?: string;
   labelStyle?: string;
   placeholder: string;
+  image?: Omit<ImageProps, 'src' | 'alt'>;
+  background?: string;
 }
 
 const cx = classNames.bind(styles);
@@ -20,11 +24,14 @@ export default function InputLayout({
   type,
   label,
   value,
-  style,
+  size,
+  border,
   isError,
   errorText,
   labelStyle,
   placeholder,
+  image,
+  background,
 }: InputProps) {
   return (
     <div className={cx('inputWithLabel')}>
@@ -33,7 +40,10 @@ export default function InputLayout({
           {label}
         </label>
       )}
-      <input id={id} type={type} value={value} className={cx({ error: isError }, style)} placeholder={placeholder} />
+      <div className={cx('inputWithIcon', size)}>
+        <input value={value} className={cx(border, { error: isError }, size, background)} placeholder={placeholder} />
+        {image && <Image src="/images/search.svg" alt="검색 아이콘" width={24} height={24} className={cx('icon')} />}
+      </div>
       {isError && <p className={cx('errorText')}>{errorText}</p>}
     </div>
   );
--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -6,15 +6,43 @@ const cx = classNames.bind(styles);
 
 export default function Input() {
   return (
-    <InputLayout
-      id=""
-      type=""
-      style={cx('large')}
-      isError={true}
-      errorText="이메일을 확인해주세요"
-      label="이메일"
-      labelStyle={cx('label')}
-      placeholder="이메일을 입력해주세요"
-    />
+    <>
+      <InputLayout
+        id=""
+        type="email"
+        size={cx('large')}
+        isError={true}
+        errorText="이메일을 확인해주세요"
+        label="이메일"
+        labelStyle={cx('label')}
+        placeholder="이메일을 입력해주세요"
+      />
+      <InputLayout
+        id=""
+        type="text"
+        size={cx('mediumLarge')}
+        border={cx('roundBorder')}
+        isError={false}
+        errorText=""
+        label=" "
+        labelStyle={cx('label')}
+        placeholder="검색어를 입력해주세요"
+        image={{}}
+        background={cx('background')}
+      />
+      <InputLayout
+        id=""
+        type="text"
+        size={cx('medium')}
+        border={cx('roundBorder')}
+        isError={false}
+        errorText=""
+        label=" "
+        labelStyle={cx('label')}
+        placeholder="검색어를 입력해주세요"
+        image={{}}
+        background={cx('background')}
+      />
+    </>
   );
 }
