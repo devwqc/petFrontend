7b87d304023faeb3a1bd41a1253229f7cb5cbb08
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-05-23 13:53:17 +0900

    refactor: 불필요한 코드 제거

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -7,42 +7,44 @@ const cx = classNames.bind(styles);
 export default function Input() {
   return (
     <>
+      <InputLayout id="" type="text" size={cx('')} label=" " labelStyle={cx('')} placeholder="" />
+    </>
+  );
+}
+
+{
+  /* 사용법
+  1. 닉네임 인풋
       <InputLayout
         id=""
         type="text"
         size={cx('large')}
-        isError={true}
-        errorText="닉네임은 30자 내외입니다"
         label=" "
         labelStyle={cx('label')}
         placeholder="닉네임을 입력해주세요"
       />
+  2.  사이즈 큰 검색 인풋
       <InputLayout
         id=""
         type="text"
         size={cx('mediumLarge')}
         border={cx('roundBorder')}
-        isError={false}
-        errorText=""
         label=" "
         labelStyle={cx('label')}
         placeholder="검색어를 입력해주세요"
         image={{}}
         background={cx('background')}
       />
+  3. 사이즈 작은 검색 인풋
       <InputLayout
         id=""
         type="text"
         size={cx('medium')}
         border={cx('roundBorder')}
-        isError={false}
-        errorText=""
         label=" "
         labelStyle={cx('label')}
         placeholder="검색어를 입력해주세요"
         image={{}}
         background={cx('background')}
-      />
-    </>
-  );
+      /> */
 }
