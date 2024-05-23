916d776ff10060e3444a46d7900d65c7c0789d17
seolsis <seoljw1005@naver.com>
2024-05-23 18:33:36 +0900

    fix: hidden input 적용 취소

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -11,28 +11,24 @@ interface DropdownProps {
   options: Option[];
   placeholder?: string;
   size: 'large' | 'small';
-  [key: string]: any;
 }
 
-export default function Dropdown({ size, options, placeholder, ...rest }: DropdownProps) {
+export default function Dropdown({ size, options, placeholder }: DropdownProps) {
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
   const sizeClass = styles[size];
 
   const handleChange = (option: Option | null) => {
     setSelectedOption(option);
-    console.log(option);
   };
 
   return (
     <>
-      <input type="hidden" value={selectedOption ? selectedOption.value : ''} {...rest} />
       <Select
         className={`${styles.dropdown} ${sizeClass}`}
         options={options}
         value={selectedOption}
         onChange={handleChange}
         placeholder={placeholder || '옵션'}
-        instanceId="unique-instance-id"
       />
     </>
   );
