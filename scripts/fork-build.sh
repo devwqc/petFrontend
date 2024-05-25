eaf3880a3d4cb2ca379e7f8a0739e637ddd7b958
devwqc <devwqc@gmail.com>
2024-05-25 07:31:39 +0000

    chore: vercel-preview.yml GitHub Actions 파일 추가

---
+++ scripts/fork-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ../
+
+mkdir output
+
+cp -R ./petFrontend/* ./output
+
+cp -R ./output ./petFrontend/
---
+++ src/pages/test/vercel/index.tsx
@@ -0,0 +1,3 @@
+export default function VercelPage() {
+  return <div>VercelPage</div>;
+}
