8e1cb592c3a774169f8c09dbcd6e35b9695b913f
devwqc <devwqc@gmail.com>
2024-05-28 02:43:46 +0000

    chore: @tanstack/react-query 패키지 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useState } from 'react';
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import '@/styles/reset.scss';
 
@@ -15,13 +17,19 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page);
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
       <Head>
-        <title>3 team</title>
+        <title>포잉마켓</title>
       </Head>
-      {getLayout(<Component {...pageProps} />)}
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          {getLayout(<Component {...pageProps} />)}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </HydrationBoundary>
+      </QueryClientProvider>
     </>
   );
 }
