fc01861cbfb36be3ba9c07efdb6563bde48f6688
devwqc <devwqc@gmail.com>
2024-06-10 04:56:55 +0000

    design: StarRating editable style 추가 및 별점 색상 변경

--- src/components/common/review/StarRating.tsx
+++ src/components/common/review/StarRating.tsx
@@ -29,7 +29,11 @@ export default function StarRating({
   const TOTAL_STARS = 5;
 
   const Star = ({ selected, onSelect }: StarProps) => (
-    <FaStar className={cx(starStyle)} color={selected ? '#FAF60D' : '#D9D9D9'} onClick={onSelect} />
+    <FaStar
+      className={cx(starStyle, { editStarStyle: editable })}
+      color={selected ? '#FFD43C' : '#F3F4F7'}
+      onClick={onSelect}
+    />
   );
 
   const handleSelect = (i: number) => {
@@ -38,11 +42,31 @@ export default function StarRating({
     }
   };
 
+  const getRatingText = (rating: number) => {
+    switch (rating) {
+      case 0:
+        return '별점을 선택해 주세요';
+      case 1:
+        return '아쉬워요';
+      case 2:
+        return '그저 그래요';
+      case 3:
+        return '괜찮아요';
+      case 4:
+        return '좋아요';
+      case 5:
+        return '최고예요';
+    }
+  };
+
   return (
-    <div className={cx('starRating', starRatingStyle)}>
-      {createArray(TOTAL_STARS).map((_, i) => (
-        <Star key={i} selected={rating > i} onSelect={() => handleSelect(i)} />
-      ))}
+    <div className={cx({ editStarRatingBoxStyle: editable })}>
+      <div className={cx('starRating', starRatingStyle, { editStarRatingStyle: editable })}>
+        {createArray(TOTAL_STARS).map((_, i) => (
+          <Star key={i} selected={rating > i} onSelect={() => handleSelect(i)} />
+        ))}
+      </div>
+      {editable && <p className={cx('ratingText')}>{getRatingText(rating)}</p>}
     </div>
   );
 }
