945928b0363a8bed5b5f8c0d68270da5ec3778a0
devwqc <devwqc@gmail.com>
2024-06-21 15:30:38 +0000

    chore: proxy 설정

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,14 @@ const nextConfig = {
     });
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://ec2-43-203-126-173.ap-northeast-2.compute.amazonaws.com/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
